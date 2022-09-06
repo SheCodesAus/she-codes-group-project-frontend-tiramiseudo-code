380d30ccc4214bfa85fdbdc8f4372594c3a6ee74
DaniZhong <102441707+DaniZhong@users.noreply.github.com>
2022-09-06 20:13:05 +1000

    Hello world
    Hello world

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import HomePage from "./pages/HomePage/index";
-
+import AllUsersPage from "./pages/AllProfilesPage/index";
+import OneUserPage from "./pages/ProfileViewPage/index";
+import HelloPage from "./pages/HelloPage"
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
       <Nav />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/AllProfiles" element={<AllUsersPage />} />
+        <Route exact path="/ProfilesView" element={<OneUserPage />} />
+        <Route exact path="/hello" element={<HelloPage />} />
       </Routes>
     </Router>
     </div>
