e22297b0faac96a305b28878a416db4c73538ef8
DaniZhong <102441707+DaniZhong@users.noreply.github.com>
2022-09-13 23:42:18 +1000

    Integrate Profile Pages and components to backend
    Integrate AllProfiles, ProfileView Pages, ProfileCard and Filter function to backend

--- src/Routes/AllyRouter.jsx
+++ src/Routes/AllyRouter.jsx
@@ -1,11 +1,17 @@
 
 import {Route, Routes} from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import AllUsersPage from "../pages/AllProfilesPage";
+import OneUserPage from "../pages/ProfileViewPage"
+import SignUpPage from "../pages/SignUpPage"
 
 function AllyRouter() {
 
     return(
         <Routes>
+            <Route exact path="/allProfiles" element={<AllUsersPage />} />
+            <Route exact path="/ProfileView/:id" element={<OneUserPage />} />
+            <Route exact path="/SignUp" element={<SignUpPage />} />
             <Route path="/" element={<HomePage />} />
         </Routes>
     );
--- src/pages/ProfileViewPage/index.jsx
+++ src/pages/ProfileViewPage/index.jsx
@@ -1,75 +1,52 @@
 
-// import React, { useState, useEffect } from "react";
-import React from "react";
-import { oneUser } from "./data";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useNavigate  } from "react-router-dom";
-// import { useParams,Link, useNavigate } from "react-router-dom";
+
  
 function OneUserPage() {
-    // When backend API is ready
-    // const [oneUserData, setOneUserData] = useState({profile: [] });
-    // const { id } = useParams();
-    // const navigate = useNavigate();
-   
-    // useEffect(() => {
-    //     fetch(`${process.env.REACT_APP_API_URL}users/${id}`)
-    //     .then((results) => {
-    //         return results.json();
-    //     })
-    //     .then((data) => {
-    //     setOneUserData(data);
-    //     });
-    // }, ); 
-
-    // return(
-    //     <div>
-    //         <img src={oneUserData.photo} alt=""/>
-    //         <h2>{oneUserData.first_name}{oneUserData.last_name}</h2>
-    //         {/* <h3>Created at: {users.date_created}</h3> */}
-    //         {/* <h3>{`Status: ${users.is_open}`}</h3> */}
-    //         <h3>Bio:{oneUserData.bio}</h3>
-    //         <h3>Skills:</h3>
-    //             <ul>
-    //                 {oneUserData.skills.map((skillsData, key) => {
-    //                     return (
-    //                     <li><img src={skillsData.icon} alt=""/> {skillsData.skill_name}
-    //                     </li>
-    //                     );
-    //                 })}
-    //             </ul>
-    //     </div>
-    // );
-
-    // use local data
+    const [oneUserData, setOneUserData] = useState({profile: [] });
+    const { id } = useParams();
 
     const navigate = useNavigate();
     const handleSubmit = (e) => {
-        e.preventDefault();
+                e.preventDefault();
+                navigate("/AllProfiles");}
 
-        navigate("/AllProfiles");}
- 
- 
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}users/${id}`)
+        .then((results) => {
+        return results.json();
+        })
+        .then((data) => {
+        setOneUserData(data);
+        
+        });
+    }, [id]);  
+  
+    if (!oneUserData.skills) {
+    return ("Data is loading")
+    }
+    
+
     return(
-       <div>
-           <img src={oneUser.photo} alt=""/>
-           <h2>{oneUser.first_name}{oneUser.last_name}</h2>
-           {/* <h3>Created at: {users.date_created}</h3> */}
-           {/* <h3>{`Status: ${users.is_open}`}</h3> */}
-           <h3>Bio:{oneUser.bio}</h3>
-           <h3>Skills:</h3>
-               <ul>
-                   {oneUser.skills.map((skillsData, key) => {
-                       return (
-                       <li><img src={skillsData.icon} alt=""/> {skillsData.skill_name}
-                       </li>
-                       );
-                   })}
-               </ul>
-            <button type="submit" onClick={handleSubmit}>Back to Home</button>
-       </div>
-   );
-}
+        <div>
+            <img src={oneUserData.photo} alt=""/>
+            <h2>{oneUserData.first_name}{oneUserData.last_name}</h2>
+            <h2>{oneUserData.pronoun }</h2>
+            <h3>{oneUserData.bio}</h3>
+            <h3>Skills:</h3>
+                <ul>
+                    {oneUserData.skills.map((skillsData, key) => {
+                        return (
+                        <li><img src={skillsData.icon} alt=""/> {skillsData.skill_name}
+                        </li>
+                        );
+                    })}
+                </ul>
+            <button type="submit" onClick={handleSubmit}>Back to all profiles page</button>
+        </div>
+    );}
 
-
- 
 export default OneUserPage;
